114e7ba610ee2b1d8b6a70c3d200dad64de070a8
Sumner1185 <alastair.sumner@yahoo.com>
2020-08-18 12:24:35 +0100

    Add function to check if special offer

--- src/shop.js
+++ src/shop.js
@@ -6,17 +6,32 @@ class Shop {
       'C': 20,
       'D': 15
     }
+    this.specialOffers = {
+      'AAA': 130,
+      'BB': 45
+    }
   }
 
   checkout = (str) => {
     const array = str.split('')
     let total = 0
-
-    for (var i = 0; i < array.length; i++) {
-      total += this.items[array[i]]
+    
+    if (this.isSpecialOffer(str)) {
+      return this.specialOffers[str]
+    } else {
+      for (var i = 0; i < array.length; i++) {
+        total += this.items[array[i]]
+      }
+      return total
     }
-    return total
   }
+
+  isSpecialOffer = (str) => {
+    if (this.specialOffers[str]) {
+      return true
+    }
+  }
+
 }
 
 module.exports = Shop
